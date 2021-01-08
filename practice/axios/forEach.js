5fb5141c605e7b9159dd2050b8199096e7e35721
songguanghui <1554095532@qq.com>
2021-01-08 17:02:17 +0800

    test axios

---
+++ practice/axios/forEach.js
@@ -0,0 +1,34 @@
+function isArray(val) {
+  return Object.prototype.toString.call(val) === '[object Array]';
+}
+
+function forEach(obj, fn) {
+  // Don't bother if no value provided
+  if (obj === null || typeof obj === 'undefined') {
+    return;
+  }
+
+  // Force an array if not already something iterable
+  if (typeof obj !== 'object') {
+    /*eslint no-param-reassign:0*/
+    obj = [obj];
+  }
+
+  if (isArray(obj)) {
+    // Iterate over array values
+    for (let i = 0, l = obj.length; i < l; i++) {
+      fn.call(null, obj[i], i, obj);
+    }
+  } else {
+    // Iterate over object keys
+    for (let key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        fn.call(null, obj[key], key, obj);
+      }
+    }
+  }
+}
+
+const res = forEach([1, 2], function(value, index, obj) {
+  console.log(value, index, obj)
+})
